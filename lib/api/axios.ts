a522dd69f6a413cbecdc99cda434f92530ee16dd
Oleksandr Haponov <olex.haponov@gmail.com>
2025-08-24 12:18:41 +0300

    fix: add Authorization header with NEXT_PUBLIC_NOTEHUB_TOKEN

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "/api", // будем обращаться к /api/notes
+  headers: {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+  },
 });
 
 export default api;
