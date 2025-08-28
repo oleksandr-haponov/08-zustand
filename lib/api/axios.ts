ec3bbbb6b07f985a9ea8747f991d63d9f36b930e
Oleksandr Haponov <olex.haponov@gmail.com>
2025-08-28 22:14:29 +0300

    Update axios.ts

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -1,10 +1,15 @@
-import axios from "axios";
+import axios, { AxiosHeaders } from "axios";
+
+const isServer = typeof window === "undefined";
+const origin =
+  isServer
+    ? process.env.NEXT_PUBLIC_SITE_URL ??
+      (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000")
+    : "";
 
 const api = axios.create({
-  baseURL: "/api", // будем обращаться к /api/notes
-  headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
-  },
+  baseURL: `${origin}/api`,
+  headers: new AxiosHeaders({ "Content-Type": "application/json" }),
 });
 
 export default api;
